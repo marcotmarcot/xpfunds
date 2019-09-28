c29d3ccfa58501a7679bce10f0af9ef3d9735334
Marco Gontijo <marcotmarcot@gmail.com>
2019-09-28 09:33:22 -0300

    tf

--- tf.py
+++ tf.py
@@ -1,112 +1,147 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pathlib
+import math
+
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
+import seaborn as sns
+
 import tensorflow as tf
 from tensorflow import keras
-import time
+from tensorflow.keras import layers
 
-EPOCHS = 500
+import numpy as np
 
 def main():
-  train_data = np.loadtxt('train_data.tsv', ndmin=2)
-  train_labels = np.loadtxt('train_labels.tsv')
-  test_data = np.loadtxt('test_data.tsv', ndmin=2)
-  test_labels = np.loadtxt('test_labels.tsv')
+  with open('get.tsv') as get:
+    funds = []
+    duration = 0
+    for line in get:
+      array = np.array(list(map(parse, line.split('\t')[5:])), dtype=float)
+      if len(array) == 0:
+        continue
+      for time in range(len(array)):
+        array[time] = array[time] / 100 + 1
+      if len(array) > duration:
+        duration = len(array)
+      funds.append(array)
+  ret = make_ret(duration, funds)
+  features = make_features(duration, funds, ret)
+  print(features)
 
-  # Remove lines with zero
-  # train_data = train_data[:, np.all(train_data != 0, axis=0)]
-  # test_data = test_data[:, np.all(test_data != 0, axis=0)]
+def parse(field):
+  return field.replace(',', '.')
 
-  # Normalize
-  mean = train_data.mean(axis=0)
-  std = train_data.std(axis=0)
-  train_data = (train_data - mean) / std
-  test_data = (test_data - mean) / std
+def make_ret(duration, funds):
+  ret = np.zeros(shape=(duration, len(funds)))
+  for f in range(len(funds)):
+    fund = funds[f]
+    ret[-1][f] = fund[0]
+    for time in range(1, len(fund)):
+      ret[-1-time][f] = ret[-time][f] * fund[time]
+    for time in range(1, len(fund)):
+      ret[-1-time][f] **= 1. / (time + 1)
+  return ret
 
-  # Model
-  model = keras.Sequential([
-      keras.layers.Dense(
-          64, activation=tf.nn.relu, input_shape=(train_data.shape[1],)),
-      keras.layers.Dense(64, activation=tf.nn.relu),
-      keras.layers.Dense(1)
-  ])
-  optimizer = tf.train.RMSPropOptimizer(0.001)
-  model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
+def make_features(duration, funds, ret):
+    features = np.zeros(shape=(duration, len(funds), len(feature_functions)))
+    for index in range(len(feature_functions)):
+      feature_functions[index](features, funds, index)
+    features /= features.max(axis=(0, 1))
+    return features
 
-  # Stop
-  early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+def set_ret(features, funds, index):
+  duration = len(features)
+  for f in range(len(funds)):
+    fund = funds[f]
+    features[-1-(len(fund)-1)][f][index] = fund[-1]
+    for time in range(len(fund)-2, -1, -1):
+      features[-1-time][f][index] = features[-1-(time+1)][f][index] * fund[time]
+    for time in range(0, len(fund) - 1):
+      features[-1-time][f][index] **= 1. / (len(fund) - time)
 
-  # Train
-  history = model.fit(
-      train_data,
-      train_labels,
-      epochs=EPOCHS,
-      validation_split=0.2,
-      verbose=0)
+def set_median(features, funds, index):
+  duration = len(features)
+  for f in range(len(funds)):
+    fund = funds[f]
+    for time in range(len(fund)-1, -1, -1):
+      s = sorted(fund[time:])
+      m = s[len(s)//2]
+      if len(s) % 2 == 0:
+        m = (s[len(s)//2] + s[len(s)//2-1]) / 2
+      features[-1-time][f][index] = m
 
-  [loss, mae] = model.evaluate(test_data, test_labels, verbose=0)
-  print("Testing set Mean Abs Error: {}".format(mae))
-  test_predictions = model.predict(test_data).flatten()
-  np.savetxt('test_predictions.tsv', test_predictions, delimiter='\t')
+def set_std_dev(features, funds, index):
+  duration = len(features)
+  for f in range(len(funds)):
+    fund = funds[f]
+    total = fund[-1]
+    for time in range(len(fund)-2, -1, -1):
+      total += fund[time]
+      count = len(fund) - time
+      avg = total / count
+      sumDiffs = 0.
+      for i in range(time, len(fund)):
+        diff = fund[time] - avg
+        sumDiffs += diff * diff
+      features[-1-time][f][index] = math.sqrt(sumDiffs / count)
 
+def set_negative_month_ratio(features, funds, index):
+  duration = len(features)
+  for f in range(len(funds)):
+    fund = funds[f]
+    negative = 0.
+    nonNegative = 0.
+    for time in range(len(fund)-1, -1, -1):
+      if fund[time] < 1:
+        negative += 1
+      else:
+        nonNegative += 1
+      features[-1-time][f][index] = negative / (negative+nonNegative)
 
-class Fund:
-  def __init__(self, line):
-    fields = line.strip().split('\t')
-    monthly = []
-    for f in fields[5:]:
-      monthly.append(1 + float(f.replace(',', '.')) / 100)
-    self.duration = len(self.raw)
-    # The monthly profit of the fund. Starts from the last month we have data
-    # and goes back to the first month we have data.
-    self.monthly = np.array(monthly)
-    self.duration = len(self.monthly)
+def set_greatest_fall(features, funds, index):
+  duration = len(features)
+  for f in range(len(funds)):
+    fund = funds[f]
+    greatestFall = 1.
+    greatestFallLen = 0.
+    curr = 1.
+    for time in range(len(fund)-1, -1, -1):
+      curr *= fund[time]
+      if fund[time] < curr:
+        curr = fund[time]
+      if curr < greatestFall:
+        greatestFall = curr
+      features[-1-time][f][index] = greatestFall
 
-  def annual(self, end, start):
-    return self.monthly[end:start].prod() ** (1.0/((start - end)/12.0))
+def set_greatest_fall_len(features, funds, index):
+  duration = len(features)
+  for f in range(len(funds)):
+    fund = funds[f]
+    greatestFall = 1.
+    greatestFallLen = 0.
+    curr = 1.
+    currLen = 0.
+    for time in range(len(fund)-1, -1, -1):
+      curr *= fund[time]
+      currLen += 1
+      if fund[time] < curr:
+        curr = fund[time]
+        currLen = 1
+      if curr < greatestFall:
+        greatestFall = curr
+        greatestFallLen = currLen
+      features[-1-time][f][index] = greatestFallLen
 
-
-def data_and_labels(funds, end, start):
-  data = []
-  labels = []
-  for f in funds:
-    for time in range(end + 1, start - 1):
-      if time >= f.duration:
-        break
-      duration = f.duration - time
-      negative_months = 0
-      largest_drop = 999.0
-      for monthly in f.monthly[time:f.duration]:
-        if monthly < 1.0:
-          negative_months += 1
-        drop = monthly
-        for s in range(e + 1, f.duration):
-          drop *= f.raw[s]
-          if drop < largest_drop:
-            largest_drop = drop
-      data.append([
-          f.annual(time, f.duration),
-          duration,
-          negative_months / duration,
-          np.array(values).std(axis=0),
-          largest_drop
-      ])
-      labels.append(f.annual(end, time))
-  return np.array(data), np.array(labels)
-
-
-def plot_history(history):
-  plt.figure()
-  plt.xlabel('Epoch')
-  plt.ylabel('Mean Abs Error')
-  plt.plot(
-      history.epoch,
-      np.array(history.history['mean_absolute_error']),
-      label='Train Loss')
-  plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']), label='Val loss')
-  plt.legend()
-  plt.ylim([0, 5])
-  plt.show()
-
+feature_functions = [
+    set_ret,
+    set_median,
+    set_std_dev,
+    set_negative_month_ratio,
+    set_greatest_fall,
+    set_greatest_fall_len,
+]
 
 if __name__ == '__main__':
   main()
