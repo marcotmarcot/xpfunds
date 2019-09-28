916ebfcd26c7fa75aeca0a1b0a4d06f88d1c2b36
Marco Gontijo <marcotmarcot@gmail.com>
2019-09-28 10:03:48 -0300

    normalize

--- tf.py
+++ tf.py
@@ -37,68 +37,45 @@ def make_ret(duration, funds):
   ret = np.zeros(shape=(duration, len(funds)))
   for f in range(len(funds)):
     fund = funds[f]
-    ret[-1][f] = fund[0]
-    for time in range(1, len(fund)):
-      ret[-1-time][f] = ret[-time][f] * fund[time]
-    for time in range(1, len(fund)):
-      ret[-1-time][f] **= 1. / (time + 1)
+    for time in range(len(fund)):
+      ret[-1-time][f] = np.prod(fund[:time+1]) ** (1. / (time+1))
   return ret
 
 def make_features(duration, funds, ret):
     features = np.zeros(shape=(duration, len(funds), len(feature_functions)))
     for index in range(len(feature_functions)):
       feature_functions[index](features, funds, index)
-    features /= features.max(axis=(0, 1))
+    features = (features - np.mean(features, axis=(0, 1))) / np.std(features, axis=(0, 1))
     return features
 
 def set_ret(features, funds, index):
   duration = len(features)
   for f in range(len(funds)):
     fund = funds[f]
-    features[-1-(len(fund)-1)][f][index] = fund[-1]
-    for time in range(len(fund)-2, -1, -1):
-      features[-1-time][f][index] = features[-1-(time+1)][f][index] * fund[time]
-    for time in range(0, len(fund) - 1):
-      features[-1-time][f][index] **= 1. / (len(fund) - time)
+    for time in range(len(fund)):
+      features[-1-time][f][index] = np.prod(fund[time:]) ** (1. / (len(fund)-time))
 
 def set_median(features, funds, index):
   duration = len(features)
   for f in range(len(funds)):
     fund = funds[f]
-    for time in range(len(fund)-1, -1, -1):
-      s = sorted(fund[time:])
-      m = s[len(s)//2]
-      if len(s) % 2 == 0:
-        m = (s[len(s)//2] + s[len(s)//2-1]) / 2
-      features[-1-time][f][index] = m
+    for time in range(len(fund)):
+      features[-1-time][f][index] = np.median(fund[time:])
 
 def set_std_dev(features, funds, index):
   duration = len(features)
   for f in range(len(funds)):
     fund = funds[f]
-    total = fund[-1]
-    for time in range(len(fund)-2, -1, -1):
-      total += fund[time]
-      count = len(fund) - time
-      avg = total / count
-      sumDiffs = 0.
-      for i in range(time, len(fund)):
-        diff = fund[time] - avg
-        sumDiffs += diff * diff
-      features[-1-time][f][index] = math.sqrt(sumDiffs / count)
+    for time in range(len(fund)):
+      features[-1-time][f][index] = np.std(fund[time:])
 
 def set_negative_month_ratio(features, funds, index):
   duration = len(features)
   for f in range(len(funds)):
     fund = funds[f]
-    negative = 0.
-    nonNegative = 0.
-    for time in range(len(fund)-1, -1, -1):
-      if fund[time] < 1:
-        negative += 1
-      else:
-        nonNegative += 1
-      features[-1-time][f][index] = negative / (negative+nonNegative)
+    for time in range(len(fund)):
+      period = fund[time:]
+      features[-1-time][f][index] = len(np.extract(period < 1, period)) / len(period)
 
 def set_greatest_fall(features, funds, index):
   duration = len(features)
